da2ddabfb4980d80c00edcb1b3f2df3e0eca983d
pfc <pflores82@gmail.com>
2020-02-16 13:20:00 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoclicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivateApp("wowclassic");
+
+            while (true)
+            { 
+                SendKeys.SendWait(" ");
+                Thread.Sleep(5000);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        static void ActivateApp(string processName)
+        {
+            Process[] p = Process.GetProcessesByName(processName);
+
+            // Activate the first application we find with this name
+            if (p.Count() > 0)
+                SetForegroundWindow(p[0].MainWindowHandle);
+        }
+    }
+}
